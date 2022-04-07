89d03003ffbf52e841d2fa818044d46be31ee7e6
Stan1slavssKy <stas00333@gmail.com>
2022-04-07 16:53:06 +0300

    add bench

---
+++ build/benchmark/CMakeFiles/run_corrector_bench.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_corrector_bench"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/run_corrector_bench.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/benchmark/CMakeFiles/run_corrector_bench.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for run_corrector_bench.
+# This may be replaced when dependencies are built.
